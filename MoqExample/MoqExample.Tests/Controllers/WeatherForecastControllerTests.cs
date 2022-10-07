2e9560552cf0c089a6f060ee602a5c54bede6f2c
bill huang <a1245bill@yahoo.com.tw>
2022-10-07 23:35:28 +0800

    feat: Moq_Example

---
+++ MoqExample/MoqExample.Tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using MoqExample.Controllers;
+using Xunit;
+
+namespace MoqExample.Tests.Controllers {
+  public class WeatherForecastControllerTests {
+    [Fact]
+    public void Get() {
+      //Arrange
+      var MockLogger = new Mock<ILogger<WeatherForecastController>>();
+      var Controllers = new WeatherForecastController(MockLogger.Object);
+      //Act
+      var Results = Controllers.Get();
+      //Assert
+      Assert.NotNull(Results);
+      Assert.Equal(5, Results.Count());
+    }
+  }
+}
