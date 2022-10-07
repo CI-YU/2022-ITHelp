cf7247905243c5054c44f0f4ec15b9a2fa0aa64d
bill huang <a1245bill@yahoo.com.tw>
2022-10-07 16:54:43 +0800

    feat: xUnit_Example

---
+++ xUnitExample/xUnitExample.Tests/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitExample.Tests {
+  public class CalculatorTests {
+    [Fact]
+    public void Add_() {
+      double Expected = 20;
+      var Actual = Calculator.Add(5, 15);
+      Assert.Equal(Expected, Actual);
+    }
+  }
+}
---
+++ xUnitExample/xUnitExample/Calculator.cs
@@ -0,0 +1,7 @@
+﻿namespace xUnitExample {
+  public static class Calculator {
+    public static double Add(int a, int b) {
+      return a + b;
+    }
+  }
+}
