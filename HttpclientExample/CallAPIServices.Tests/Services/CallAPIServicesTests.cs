5705ef14db01815fd8da3cbcf6ab4e9e5458af19
bill huang <a1245bill@yahoo.com.tw>
2022-10-07 23:35:57 +0800

    feat: HttpClient_Example

---
+++ HttpclientExample/CallAPIServices.Tests/Services/CallAPIServicesTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using Moq.Protected;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CallAPIServices.Tests.Services {
+  public class CallAPIServicesTests {
+    [Fact]
+    public async Task CallAPIServices_Get_HttpClient_Success() {
+      var mockFactory = new Mock<IHttpClientFactory>();
+      HttpResponseMessage result = new HttpResponseMessage() {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent("{'account':'bill','age':18}")
+      };
+      var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+      mockHttpMessageHandler.Protected()
+          .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+          .ReturnsAsync(result);
+
+      var client = new HttpClient(mockHttpMessageHandler.Object);
+      mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+
+      var services = new HttpclientExample.Services.CallAPIServices(mockFactory.Object);
+
+      var response = await services.Get();
+      Assert.NotNull(response);
+      Assert.Equal("{'account':'bill','age':18}", response);
+    }
+  }
+}
---
+++ HttpclientExample/HttpclientExample/Services/CallAPIServices.cs
@@ -0,0 +1,18 @@
+﻿namespace HttpclientExample.Services {
+  public class CallAPIServices {
+    readonly IHttpClientFactory _httpClientFactory;
+    public CallAPIServices(IHttpClientFactory httpClientFactory) {
+      _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<string> Get() {
+      var client = _httpClientFactory.CreateClient();
+      var response = await client.GetAsync("https://example.com");
+      if (response.IsSuccessStatusCode) {
+        var responseString = await response.Content.ReadAsStringAsync();
+        return responseString;
+      }
+      return "";
+    }
+  }
+}
